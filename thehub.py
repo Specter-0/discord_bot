19fb965aa225a31626f7cb9c88d09e52edc4a011
Specter-0 <121578250+Specter-0@users.noreply.github.com>
2023-03-13 23:47:03 +0100

    mby not pg for school
    you know what i mean

--- thehub.py
+++
@@ -1,7 +0,0 @@
-import random as rn, pornhub 
-from pornhub import PornHub as hub
-client = hub()
-
-async def get_star():
-    for star in client.getStars(1, rn.randint(1, 100), False):
-        return star
